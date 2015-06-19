f0703e594b3d13e12e6d8e634331abd1ea935620
Xavier Gonzalez <xavier@gonzalez.pm>
2015-06-19 19:33:11 +0200

    first commit

---
+++ inc/inc.config.php
@@ -0,0 +1,15 @@
+<?php
+define("DBHOST", "localhost");
+define("DBNAME", "accleaner");
+define("DBUSER", "accleaner");
+define("DBPASSWD", "accleaner33");
+
+// CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES
+//mail parametrage
+$mailCustomer = "u.calmo@accleaner.eu";
+$mailNameCustomer = "AcCleaner";
+
+
+//mail d'envoi
+$mailContact = "fjavi.gonzalez@gmail.com";
+$mailBcc = "xav335@hotmail.com,xavier.gonzalez@laposte.net,jav_gonz@yahoo.com";
