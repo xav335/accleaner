6953cce840fb5c03f1f12f5a9a6c08db6c1e172d
root <root@vmprod1.iconeo.fr>
2015-06-24 17:46:30 +0200

    correction detail journal + desactivaton suppression

--- inc/inc.config.php
+++ inc/inc.config.php
@@ -7,9 +7,10 @@ define("DBPASSWD", "accleaner33");
 // CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES
 //mail parametrage
 $mailCustomer = "u.calmo@accleaner.eu";
-$mailNameCustomer = "AcCleaner";
+$mailNameCustomer = "ACcleaner";
 $facebookLink = "https://www.facebook.com/ACcleanerBordeaux";
 
 //mail d'envoi
-$mailContact = "fjavi.gonzalez@gmail.com";
-$mailBcc = "xav335@hotmail.com,xavier.gonzalez@laposte.net,jav_gonz@yahoo.com";
+//$mailContact = "fjavi.gonzalez@gmail.com";
+$mailContact = "u.calmo@accleaner.eu";
+$mailBcc = "fjavi.gonzalez@gmail.com,fredericlesca@iconeo.fr";
